8f9e728029642c1d3406930dc1160c2009d70caf
Agatha <agathatran91.gmail.com>
2023-02-14 15:04:09 +0900

    Update exercise 2

--- class_exercise2.py
+++ class_exercise2.py
@@ -23,20 +23,54 @@ class Math_Operation:
         self.value_1 = value_1
         self.value_2 = value_2
 
-    def func_1(self):
+    def __func_1(self):
         print(f'Type of value_1: {type(self.value_1)}')
         print(f'Type of value_2: {type(self.value_2)}')
 
-    def func_2(self):
+    def __func_2(self):
         try:
-            sum = value_1 + value_2
+            sum = self.value_1 + self.value_2
         except TypeError:
             print("Type Error!")
         
-    def func_3(self):
-        if (type(value_1) = list) and (type(value_2) = list) and (len(value_1)=len(value_2)):
-            list_3= [x1+x2 for (x1, x2) in zip(value_1, value_2)]
+    def __func_3(self):
+        if ((isinstance(self.value_1,list)) and (isinstance(self.value_2,list)) and (len(self.value_1)==len(self.value_2))):
+            list_3= [x1+x2 for (x1, x2) in zip(self.value_1, self.value_2)]
         else:
             print("Cannot create new list")
-    
 
+#Phươpng thức 4: Nếu 2 thuộc tính có kiểu string (cùng số lượng kí tự) thì tạo ra một string thứ 3 bằng cách xen kẽ từng kí tự của 2 string
+    def __func_4(self):
+        if (isinstance(self.value_1,str) and isinstance(self.value_2,str)):
+            lst1=self.value_1.split()
+            lst2 = self.value_2.split()
+            newlst=[i+j for (i,j) in zip(lst1,lst2)]
+            newstr = ''.join(newlst)
+            print(f'The new list is: {newstr}')
+
+#Phương thức 5: Gán giá trị mới cho 2 thuộc tính bằng các giá trị nhập từ bàn phím
+    def func_5(self, value_1_, value_2_):
+        self.value_1 = value_1_
+        self.value_2 = value_2_
+        return self.value_1, self.value_2
+
+# Phương thức 6: Hiển thị tổng của 2 thuộc tính
+    def func_6(self):
+        sum = self.value_1 + self.value_2
+        return sum 
+
+# Phương thức 7: Hiển thị list thứ 3 trong trường hợp 2 thuộc tính có kiểu list
+    def func_7(self):
+        if ((isinstance(self.value_1,list)) and (isinstance(self.value_2,list)) and (len(self.value_1)==len(self.value_2))):
+            list_3= [x1+x2 for (x1, x2) in zip(self.value_1, self.value_2)]
+        else:
+            print("Cannot create new list")
+
+# Phương thức 8: Hiển thị string thứ 3 trong trường hợp 2 thuộc tính có kiểu string
+    def func_8(self):
+        if (isinstance(self.value_1,str) and isinstance(self.value_2,str)):
+            lst1=self.value_1.split()
+            lst2 = self.value_2.split()
+            newlst=[i+j for (i,j) in zip(lst1,lst2)]
+            newstr = ''.join(newlst)
+            print(f'The new list is: {newstr}')
